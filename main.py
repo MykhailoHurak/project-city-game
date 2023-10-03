96091cd3b980fc0a73ab960b9bc92bb4d8fb6c8c
mikegurak <92752760+MykhailoHurak@users.noreply.github.com>
2023-10-03 22:55:07 +0300

    update

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from random import choice
+
+last_computer_letter = None
 
 def check_city():
+    global last_computer_letter
     city = entry.get().title()
-    response = f"Python гра: Ви ввели місто {city}\n"
+    if not city:
+        response = f"Ви не ввели місто"
+    elif city in used_cities:
+        response = f"Місто {city} вже було використано"
+    elif city not in cities:
+        response = f"Місто {city} відсутнє в переліку міст, які знає гра"
+    elif last_computer_letter and city[0] !=last_computer_letter:
+        response = f"Місто понно починатися на літеру {last_computer_letter}"
+    else:
+        used_cities.append(city)
+        last_letter = get_last_letter(city)
+        # last_letter = city[-1].upper()
+        # if last_letter in ["Ь", "Й", "И"]:
+        #     last_letter = city[-2].upper()
+        computer_city = get_city_by_letter(last_letter)
+        if not computer_city:
+            response = f"Ви виграли! Я не знаю міст на літеру {last_letter}"
+        else:
+            used_cities.append(computer_city)
+            last_computer_letter = get_last_letter(computer_city)
+            response = f"Мій варіант - це місто {computer_city}"
+
+    # response = f"Python гра: Ви ввели місто {city}\n"
     game_log.config(state=tk.NORMAL)
     game_log.insert(tk.END, f"Ви: {city}\n")
-    game_log.insert(tk.END, response)
+    game_log.insert(tk.END, f"Гра: {response}\n")
     game_log.yview(tk.END)
     game_log.config(state=tk.DISABLED)
     notification.config(text=response)
     entry.delete(0, tk.END)
 
+def get_last_letter(city):
+    last_letter = city[-1].upper()
+    if last_letter in ["Ь", "Й", "И"]:
+        last_letter = city[-2].upper()
+    return last_letter
+
+def get_city_by_letter(letter):
+    available_cities = []
+    for city in cities:
+        if city[0].upper() == letter and city not in used_cities:
+            available_cities.append(city)
+            if available_cities:
+                return choice(available_cities)
+
+cities = ["Київ", "Вінниця", "Ялта", "Алчевськ", "Кривий Ріг", "Глухів", "Вишгород", "Дніпро", "Обухів", "Васильків", "Вишневе", "Енергодар", "Радомишль", "Луцьк", "Кропивницький"]
+used_cities = []
 
 root = tk.Tk()
 root.title('Гра в міста')
@@ -28,6 +70,9 @@ entry.pack(padx=10, pady=10)
 
 button = tk.Button(left_frame, text='Відправити', command=check_city)
 button.pack(padx=10, pady=10)
+
+button_end = tk.Button(left_frame, text='Python win', command=check_city)
+button_end.pack(padx=10, pady=10)
 
 notification = tk.Label(left_frame, text='')
 notification.pack(padx=10, pady=10)
